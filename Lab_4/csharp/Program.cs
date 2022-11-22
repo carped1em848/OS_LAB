f089784ce88d45800d39f0abc5e8dfb777a8d2ac
carped1em848 <61061813+carped1em848@users.noreply.github.com>
2022-11-22 16:49:02 +0300

    Add files via upload

---
+++ Lab_4/csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+namespace CSharp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 2;
+            int c = 3;
+            long iterations = 100000000l;
+
+            DateTime start = DateTime.Now;
+            
+            for (int i = 0; i <= iterations; i++) {
+                a += b * 2 + c - i;
+            }
+            Console.WriteLine(DateTime.Now - start);
+        }
+    }
+}
